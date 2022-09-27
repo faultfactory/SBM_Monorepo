2ee27d0b935ed27f42c8d5241399f8265487652f
Jonwhite002 <jonathon.white@stonybrook.edu>
2022-09-27 17:27:06 -0400

    Updated logger button ino

--- Nodes/LOGGER_BUTTON/LOGGER_BUTTON.ino
+++ Nodes/LOGGER_BUTTON/LOGGER_BUTTON.ino
@@ -7,8 +7,9 @@ can_obj_sbm_network_definition_h_t can_obj;
 millisDelay Button_Press; 
 bool Logger_Read = false;
 int buttonState = 0;
-
 const int buttonPin = 12;
+
+
 
 union MsgData
 {
@@ -27,54 +28,79 @@ void setup() {
   Serial.begin(115200);
   CAN.begin(500000); // start the CAN bus at 500 kbps (kilo-bits/second)
 
-  Button_Press.start(50);
 }
 
-void loop() {
-  if(Button_Press.justFinished())
+void loop() 
+{
+  bool sendCan = false;
+
+  if(digitalRead(buttonPin) == 1)
   {
-    Button_Press.repeat();
-
-    bool buttonDown = false;
-    if (buttonState != digitalRead(buttonPin) && digitalRead(buttonPin) == 1) {
-      buttonDown = true;
+    if(!buttonState)
+    {
+      Button_Press.start(50);
+      buttonState=true;
+    }
+    else
+    {
+      if(Button_Press.justFinished())
+      {
+         sendCAN(can_obj, Logger_Read);
+      }
+    }
   }
-    buttonState = digitalRead(buttonPin);
-
-    encode_can_0x104_CAN_Logger(&can_obj,1);
-    encode_can_0x105_CAN_Logger(&can_obj,1); 
-
-    MsgData msg_102, msg_104, msg_105;  
-
-    //DEBUG
-    double accel = 5.3;
-    Serial.println(accel);
-    encode_can_0x102_Raw_Accel_x_mps2(&can_obj,accel);
-    pack_message(&can_obj,0x102,&msg_102.a);
-    CAN.beginPacket(0x102,8,false);
-    CAN.write(msg_102.b,8);
-    CAN.endPacket();
-    //END DEBUG
-  
-    /* Button pressed when logger is off */
-    if (buttonDown & !Logger_Read){
-      Logger_Read = true;
-      
-      Serial.println("Send logger START");
-      pack_message(&can_obj,0x105,&msg_105.a);
-      CAN.beginPacket(0x105,8,false);
-      CAN.write(msg_105.b,8);
-      CAN.endPacket();  
+  else
+  {
+    if(buttonState)
+    {
+      buttonState = false;
+      Button_Press.finish();      
     }
-    /* Button pressed when logger is on */
-    else if (buttonDown & Logger_Read){
-      Logger_Read = false;
-      
-      Serial.println("Send logger STOP");
-      pack_message(&can_obj,0x104,&msg_104.a);
-      CAN.beginPacket(0x104,8,false);
-      CAN.write(msg_104.b,8);
-      CAN.endPacket();  
+    else
+    {
+      if(Button_Press.isRunning())
+      {
+        Button_Press.finish();
+      }
     }
+  }
+}
+
+void sendCAN(can_obj_sbm_network_definition_h_t &can_obj, bool &Logger_Read) 
+{
+  encode_can_0x104_CAN_Logger(&can_obj,1);
+  encode_can_0x105_CAN_Logger(&can_obj,1); 
+
+  MsgData msg_102, msg_104, msg_105;  
+
+  //DEBUG
+  double accel = 5.3;
+  Serial.println(accel);
+  encode_can_0x102_Raw_Accel_x_mps2(&can_obj,accel);
+  pack_message(&can_obj,0x102,&msg_102.a);
+  CAN.beginPacket(0x102,8,false);
+  CAN.write(msg_102.b,8);
+  CAN.endPacket();
+  //END DEBUG
+
+  /* Button pressed when logger is off */
+  if (!Logger_Read){
+    Logger_Read = true;
+    
+    Serial.println("Send logger START");
+    pack_message(&can_obj,0x105,&msg_105.a);
+    CAN.beginPacket(0x105,8,false);
+    CAN.write(msg_105.b,8);
+    CAN.endPacket();  
+  }
+  /* Button pressed when logger is on */
+  else if (Logger_Read){
+    Logger_Read = false;
+    
+    Serial.println("Send logger STOP");
+    pack_message(&can_obj,0x104,&msg_104.a);
+    CAN.beginPacket(0x104,8,false);
+    CAN.write(msg_104.b,8);
+    CAN.endPacket();  
   }
 }
