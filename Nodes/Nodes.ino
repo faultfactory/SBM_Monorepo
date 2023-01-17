0f6b3685feb4409dcde258753d201d570ec1db62
samar-01 <66644907+samar-01@users.noreply.github.com>
2023-01-17 08:25:07 -0500

    Continued reorganization and now you must switch over to vscode (#25)
    * added Makefile and compile script for CI purposes. Will soon add a tasks.json for vscode
    
    * Fixed compile.sh so that it will fail completely even if part of it doesn't compile
    
    * Added tasks.json for vscode so now it should build, will need to add automatic uploading to board
    
    * vscode tasks includes uploading to board (untested)

--- Nodes/Nodes.ino
+++ Nodes/Nodes.ino
@@ -1,3 +1,9 @@
+#ifdef FRONT_NODE
 #include "Front_Node.h"
-// #include "Screen_Node.h"
-// #include "Vehicle_Dyamics_Node.h"
+#endif
+#ifdef SCREEN_NODE
+#include "Screen_Node.h"
+#endif
+#ifdef VEHICLE_DYNAMICS_NODE
+#include "Vehicle_Dyamics_Node.h"
+#endif
